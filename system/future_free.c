ff5825eb96b2e718517e693576bd9081fccd5d5d
pooja <poosingh@iu.edu>
2016-10-04 15:16:40 -0400

    assignment-4 changes

---
+++ system/future_free.c
@@ -0,0 +1,10 @@
+#include<xinu.h>
+#include<future.h>
+syscall future_free(future* f){
+int status=0;
+status+=freemem((char* )f,sizeof(future));
+if(status==3)
+   return OK;
+else
+   return SYSERR;
+}
